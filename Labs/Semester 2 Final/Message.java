abe0e79489d44c2132777915ec1f5620169ff8c6
Neil Balch <neilbalch@gmail.com>
2020-05-22 13:52:39 -0700

    Make chance of pulling a 1 or 2 card far 3x greater for Semester 2 Final Project.

--- Labs/Semester 2 Final/Message.java
+++ Labs/Semester 2 Final/Message.java
@@ -7,7 +7,15 @@ public class Message implements Serializable {
         ONE, TWO, THREE, FOUR, FIVE, SEVEN, EIGHT, TEN, ELEVEN, TWELVE, SORRY;
 
         public static Card selectCard() {
-            return Card.values()[(int) (Math.random() * Card.values().length)];
+            int multipleOne = 2; // ONE will be selected 2x more than normal.
+            int multipleTwo = 2; // TWO will be selected 2x more than normal.
+            int selection = (int) (Math.random() * ((1 + multipleOne) + (1 + multipleTwo) + Card.values().length - 2));
+
+            if (selection < (multipleOne + 1)) return Card.values()[0];
+            else if (selection < (multipleOne + 1 + multipleTwo + 1)) return Card.values()[1];
+            else return Card.values()[selection - multipleOne - multipleTwo];
+
+//            return Card.values()[(int) (Math.random() * Card.values().length)];
         }
     }
 
